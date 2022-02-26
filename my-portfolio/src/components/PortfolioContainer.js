fe30df1ce1c8e2025e53af7d8b8279119c0a0c31
Juno <junonguyen27@gmail.com>
2022-02-25 19:35:08 -0600

    attempting to make nav tabs

---
+++ my-portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Portfolio from './pages/Portfolio';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // TODO: Add a comment describing the functionality of this method
+  // depending on what "page" your are on within the virtual dom, render different tabs
+  const renderPage = () => {
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Blog') {
+      return <Blog />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+    </div>
+  );
+}
