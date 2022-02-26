0227e31c13b6b2c959892dc31c76f4703099545e
Juno <junonguyen27@gmail.com>
2022-02-25 19:21:24 -0600

    added repos page

--- my-portfolio/src/components/Portfolio.js
+++ my-portfolio/src/components/Portfolio.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import './Reset.css';
+import './Portfolio.css';
+import JATE from './images/JATE.PNG';
+import './images/placeholder.jpg';
+import workboard from './images/workboard.PNG';
+import COVIDTRACKER from './images/COVID-TRACKER.PNG';
+import notetaker from './images/notetaker.PNG';
+import techblog from './images/techblog.PNG';
+import gist from './images/gist.PNG';
 
 function Portfolio() {
     // ADD REPO LINKS 
@@ -6,10 +15,6 @@ function Portfolio() {
     return (
         <div>
             <nav>
-                <h1>
-                    Juno Nguyen
-                </h1>
-
                 <ul>
                     <li>
                         <a href="https://github.com/JunoNguyen">Github</a>
@@ -26,58 +31,48 @@ function Portfolio() {
                 </ul>
             </nav>
             <div class="body1">
-                <header id="ninja">
-                    <h2 id="intro">
-                        Intro
-                    </h2>
-                    <p>
-                        Hello my name is Juno. I am a student looking for future opportunities within the technological industry.
-                        I have grown up around computers and technology my entire life; practically everyone in my family works in the technical field.
-                        Currently, I am taking a bootcamp to learn many skills that pertain to technology, and I am always looking to improve my skillset.
-                    </p>
-                </header>
-
                 <div class="allimages">
                     <div class="imagebox">
-                        <a href="https://skruphold.github.io/Project-1/"><img src="https://media.sproutsocial.com/uploads/2015/04/What-is-an-API.png" alt="repo-img"></img></a>
+                        <a href="https://protected-shelf-14357.herokuapp.com/"><img src={techblog} alt="repo-img" class="images"></img></a>
                         <div>
-                            <h2>Ticket Safety</h2>
-                            <p>Server Side APIs</p>
+                            <h2>Tech Blog</h2>
+                            <p>Full Stack App</p>
                         </div>
                     </div>
                     <div class="imagebox">
-                        <a href="https://junonguyen.github.io/weather-dashboard/"><img src="https://nordicapis.com/wp-content/uploads/5-Best-Free-and-Paid-Weather-APIs-2019-e1587582023501.png" alt="repo-img"></img></a>
+                        <a href="https://skruphold.github.io/Project-1/"><img src={COVIDTRACKER} alt="repo-img" class="images"></img></a>
                         <div>
-                            <h2>Weather Dashboard</h2>
-                            <p>Local Storage</p>
+                            <h2>Covid Tracker</h2>
+                            <p>Full Stack App</p>
                         </div>
                     </div>
                     <div class="imagebox">
-                        <a href="https://junonguyen.github.io/workday-scheduler/"><img src="assets/images/Capture.PNG" alt="repo-img"></img></a>
+                        <a href="https://shielded-escarpment-21478.herokuapp.com/"><img src={notetaker} alt="repo-img" class="images"></img></a>
                         <div>
-                            <h2>Day Planner</h2>
-                            <p>Local APIs</p>
+                            <h2>Note Taker</h2>
+                            <p>Full Stack App</p>
                         </div>
                     </div>
                     <div class="imagebox">
-                        <a href="https://junonguyen.github.io/first-homework/"><img src="https://feedsportal.com/wp-content/uploads/2021/01/improving-efficiency.png" alt="repo-img"></img></a>
+                        <a href="https://grammar-genius.herokuapp.com/"><img src={JATE} alt="JATE" class="images"></img></a>
                         <div>
-                            <h2>Efficiency</h2>
-                            <p>Source Code Editing</p>
+                            <h2>J.A.T.E.</h2>
+                            <p>PWA</p>
                         </div>
                     </div>
                     <div class="imagebox">
-                        <a href="https://workboard-uofm-proj2.herokuapp.com/login"><img src="assets/images/workboard.PNG" alt="repo-img"></img></a>
+                        <a href="https://workboard-uofm-proj2.herokuapp.com/login"><img src={workboard} alt="repo-img" class="images"></img></a>
                         <div>
                             <h2>Workboard</h2>
                             <p>Full Stack App</p>
                         </div>
                     </div>
-                </div>
-                <div id="apicontainer">
-                    <div id="btncontainer">
-                        <button id="repobutton">List of my Repos</button>
-                        <ul id="repos"></ul>
+                    <div class="imagebox">
+                        <a href="https://gist.github.com/JunoNguyen/c5a514447dae2c279ec8c1fe81da3942"><img src={gist} alt="repo-img" class="images"></img></a>
+                        <div>
+                            <h2>REGEX Gist</h2>
+                            <p>Gist</p>
+                        </div>
                     </div>
                 </div>
                 <div class="footer">
